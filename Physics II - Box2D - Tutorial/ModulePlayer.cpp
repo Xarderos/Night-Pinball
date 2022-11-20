10ee37aae395180752a865c9ba1ebf5e8eadeddd
Xarderos <99949891+Xarderos@users.noreply.github.com>
2022-11-20 20:30:31 +0100

    Update ModulePlayer.cpp

--- Physics II - Box2D - Tutorial/ModulePlayer.cpp
+++ Physics II - Box2D - Tutorial/ModulePlayer.cpp
@@ -168,6 +168,7 @@ bool ModulePlayer::Start()
 	gravity = -18.0;
 	GameOver = false;
 	changegrav = true;
+	oai = false;
 	LOG("Loading player");
 	return true;
 }
@@ -454,12 +455,12 @@ update_status ModulePlayer::Update()
 	if (boss == true && bossvida>=0) {
 		if (oai == false) {
 			oai = true;
-			slowtimer = 894;
+			slowtimer = 890;
 			App->audio->PlayMusic("pinball/HuntingDrowningKurapika.ogg");
 
 		}
 		if (slowtimer > 0) {
-			App->physics->velocity = 8000.0f;
+			App->physics->velocity = 7600.0f;
 
 		}
 		if(slowtimer==0)
