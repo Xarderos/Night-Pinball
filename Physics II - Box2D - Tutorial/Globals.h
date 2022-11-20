a87d0ee38d1d6a2eea03f475c29ffa04018cb221
Xarderos <99949891+Xarderos@users.noreply.github.com>
2022-11-20 19:55:05 +0100

    .exe xd

--- Physics II - Box2D - Tutorial/Globals.h
+++ Physics II - Box2D - Tutorial/Globals.h
@@ -25,8 +25,8 @@ enum update_status
 #define SCREEN_WIDTH 356
 #define SCREEN_HEIGHT 432
 #define SCREEN_SIZE 2.4
-#define WIN_FULLSCREEN false
-#define WIN_RESIZABLE false
+#define WIN_FULLSCREEN true
+#define WIN_RESIZABLE true
 #define WIN_BORDERLESS false
 #define WIN_FULLSCREEN_DESKTOP false
 #define VSYNC true
